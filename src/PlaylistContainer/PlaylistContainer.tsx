d9357acf72a04b792a36b889909914c0e1d3aa45
Nathan Darrington <npdarrington@icloud.com>
2020-10-28 16:01:33 -0600

    Added initial file structure for Playlist Container to ensure it renders correctly

---
+++ src/PlaylistContainer/PlaylistContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PlaylistContainer() {
+  return (
+    <section className='playlist-container'>
+      <h1>Playlist Container</h1>
+    </section>
+  )
+}
+
+export default PlaylistContainer;
