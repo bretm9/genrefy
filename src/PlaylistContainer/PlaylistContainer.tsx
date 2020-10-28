80180d89494fdc2369201718326c5b21cfcf204a
Nathan Darrington <npdarrington@icloud.com>
2020-10-28 16:24:39 -0600

    Added displayPlaylist that will conditionally render what is passed in as a prop

--- src/PlaylistContainer/PlaylistContainer.tsx
+++ src/PlaylistContainer/PlaylistContainer.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-function PlaylistContainer() {
+const displayPlaylist = (playlistType: string) => {
+  if (playlistType === 'saved-playlist') {
+    return <h1>Saved Playlist</h1>
+  }
+  if (playlistType === 'generated-playlist') {
+    return <h1>Generated Playlist</h1>
+  }
+  if (playlistType === 'custom-playlist') {
+    return <h1>Custom Playlist</h1>
+  }
+}
+
+function PlaylistContainer(props: any) {
   return (
     <section className='playlist-container'>
-      <h1>Playlist Container</h1>
+      { displayPlaylist(props.playlistType) }
     </section>
   )
 }
