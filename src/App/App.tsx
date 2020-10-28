227f1eb2376264b6b108d86533198e0812b2133e
Nathan Darrington <npdarrington@icloud.com>
2020-10-28 16:01:57 -0600

    Added render path for home page that will show all components based on wireframe

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,6 +4,7 @@ import { Link, Route, NavLink} from 'react-router-dom';
 
 import Header from '../Header/Header';
 import SavedPlaylist from '../SavedPlaylists/SavedPlaylist';
+import PlaylistContainer from '../PlaylistContainer/PlaylistContainer';
 
 class App extends Component<{}> {
 	constructor(props: any) {
@@ -19,7 +20,15 @@ class App extends Component<{}> {
 			<div className='App'>
 				<h1>Genrefy</h1>
 				<Header />
-				<Route exact path='/' render={() => <SavedPlaylist />} />
+				<Route exact path='/' render={() => {
+					return (
+						<section className="testing">
+							<PlaylistContainer /> {/* Saved Playlist */}
+							<PlaylistContainer /> {/* Custom Playlist */}
+							<PlaylistContainer /> {/* Generated Playlist */}
+						</section>
+					)
+				}} />
 				<Route path='/saved-playlist' render={() => <SavedPlaylist />} />
 			</div>
 		)
