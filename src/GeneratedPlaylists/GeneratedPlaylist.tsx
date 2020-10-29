6de771a93b588f77e856fd343e1b26027dc70483
Nathan Darrington <npdarrington@icloud.com>
2020-10-29 15:23:57 -0600

    Added getPlaylist into GeneratedPlaylist to test API call with 300 entries

--- src/GeneratedPlaylists/GeneratedPlaylist.tsx
+++ src/GeneratedPlaylists/GeneratedPlaylist.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
+import { getPlaylist } from '../apiCalls'
+
+const getSongsByGenre = () => {
+  // getPlaylist('hip+hop').then(data => console.log(data));
+}
+
 function GeneratedPlaylist() {
+  console.log(getSongsByGenre());
   return (
     <section className='generated-playlist'>
     </section>
