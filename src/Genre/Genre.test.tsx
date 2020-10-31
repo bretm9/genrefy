08788a874977216489bb77bd68511e94c8c5b7d9
Nathan Darrington <npdarrington@icloud.com>
2020-10-31 11:44:31 -0600

    Test Genre component should render a genre to the screen

--- src/Genre/Genre.test.tsx
+++ src/Genre/Genre.test.tsx
@@ -5,4 +5,22 @@ import '@testing-library/jest-dom';
 
 import Genre from './Genre';
 
+const mockSelectedGenre = jest.fn();
 
+describe('Genre', () => {
+	beforeEach(() =>
+		render(
+			<Genre updateSelectedGenre={mockSelectedGenre} genre={'jesus funk'} />
+		)
+	);
+	test('should render a genre to the screen', () => {
+		expect(screen.getByText('jesus funk')).toBeInTheDocument();
+	});
+
+	test('should render another genre', () => {
+		render(
+			<Genre updateSelectedGenre={mockSelectedGenre} genre={'kraut mallet'} />
+		);
+		expect(screen.getByText('kraut mallet')).toBeInTheDocument();
+	});
+});
