6f9848ea7237cd28eec21493f956ef95be564d04
Nathan Darrington <npdarrington@icloud.com>
2020-10-31 11:44:04 -0600

    Added imports for testing

---
+++ src/Genre/Genre.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import Genre from './Genre';
+
+
