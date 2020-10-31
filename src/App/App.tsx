7e9ad3d65cd889d7001be0ab9e01c02d038e7a82
Bret Merritt <merrittbret9@gmail.com>
2020-10-31 15:11:54 -0600

    Move apiCall functions to componentDidMount and setAppGenre

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,14 +6,14 @@ import Header from '../Header/Header';
 import GenresList from '../GenresList/GenresList';
 import PlaylistContainer from '../PlaylistContainer/PlaylistContainer';
 
-import { getPlaylist } from '../apiCalls';
+import { getPlaylist, getGenres } from '../apiCalls';
 
 import { CleanedAlbumTrack } from '../utils';
 
 interface IProps {}
 interface IState {
 	genres: [],
-	playlists: [CleanedAlbumTrack[]] | [],
+	playlists: any,
 	selectedGenre: string
 }
 
@@ -23,17 +23,19 @@ class App extends Component<IProps, IState> {
 		this.state = {
 			genres: [],
 			playlists: [],
-			selectedGenre: "hip hop"
+			selectedGenre: ""
 		}
 	}
 
 	componentDidMount = () => {
-		getPlaylist(this.state.selectedGenre)
-		.then(data => this.setState({ playlists: [[...data]] }));
+		getGenres()
+    .then(data => this.setState({ genres: data}))
 	}
 
 	setAppGenre = (genre: string) => {
 		this.setState({ selectedGenre: genre })
+		getPlaylist(this.state.selectedGenre)
+		.then(data => this.setState({ playlists: [...this.state.playlists, [...data]] }));
 	}
 	
 	render() {
@@ -44,7 +46,7 @@ class App extends Component<IProps, IState> {
 				<Route exact path='/' render={() => {
 					return (
 						<section className="testing">
-							<GenresList setAppGenre={this.setAppGenre} />
+							<GenresList setAppGenre={this.setAppGenre} selectedGenre={this.state.selectedGenre} genres={this.state.genres} />
 							<PlaylistContainer playlistType={'generated-playlist'} selectedGenre={this.state.selectedGenre} playlists={this.state.playlists} />
 							{/* <PlaylistContainer playlistType={'custom-playlist'} /> */}
 						</section>
