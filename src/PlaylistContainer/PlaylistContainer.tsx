96286ce96e3754bf9fea3515ce998878c9aaae53
Nathan Darrington <npdarrington@icloud.com>
2020-10-31 12:58:21 -0600

    moved displayPlaylist inside function PlaylistContainer to have access to selectedGenre prop

--- src/PlaylistContainer/PlaylistContainer.tsx
+++ src/PlaylistContainer/PlaylistContainer.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import SavedPlaylist from '../SavedPlaylists/SavedPlaylist';
 import GeneratedPlaylist from '../GeneratedPlaylists/GeneratedPlaylist';
-import CustomPlaylist from '../CustomPlaylists/CustomPlaylist';
+// import CustomPlaylist from '../CustomPlaylists/CustomPlaylist';
 
 enum PlaylistTypes {
 	SavedPlaylist = 'saved-playlist',
 	GeneratedPlaylist = 'generated-playlist',
-	CustomPlaylist = 'custom-playlist',
+	// CustomPlaylist = 'custom-playlist',
 }
 
-const displayPlaylist = (playlistType: PlaylistTypes) => {
-	if (playlistType === PlaylistTypes.SavedPlaylist) {
-		return <SavedPlaylist />;
-	}
-	if (playlistType === PlaylistTypes.GeneratedPlaylist) {
-		return <GeneratedPlaylist />;
-	}
-	if (playlistType === PlaylistTypes.CustomPlaylist) {
-		return <CustomPlaylist />;
-	}
-};
-
 function PlaylistContainer(props: any) {
+	const displayPlaylist = (playlistType: PlaylistTypes) => {
+		if (playlistType === PlaylistTypes.SavedPlaylist) {
+			return <SavedPlaylist />;
+		}
+		if (playlistType === PlaylistTypes.GeneratedPlaylist) {
+			return <GeneratedPlaylist selectedGenre={props.selectedGenre}/>;
+		}
+		// if (playlistType === PlaylistTypes.CustomPlaylist) {
+		// 	return <CustomPlaylist />;
+		// }
+	};
+
 	return (
 		<section className='playlist-container'>
 			{displayPlaylist(props.playlistType)}
