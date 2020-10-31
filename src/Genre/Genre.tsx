df485e0874628d30a8b83c999b1f7f95136b05e4
Nathan Darrington <npdarrington@icloud.com>
2020-10-31 11:12:13 -0600

    AddedInterface for props that Genre will use and added onClick event to h1 to update Genres state

--- src/Genre/Genre.tsx
+++ src/Genre/Genre.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 interface IProps {
-  updateSelectedGenre: (genre: string) => void;
-  genre: string;
+	updateSelectedGenre: (genre: string) => void;
+	genre: string;
 }
 
 function Genre(props: IProps) {
-  console.log(props);
-  return (
-    <section className='genre'>
-      <h1>{props.genre}</h1>
-    </section>
-  )
+	return (
+		<section className='genre'>
+			<h1
+				onClick={event =>
+					props.updateSelectedGenre(event.currentTarget.innerText)
+				}>
+				{props.genre}
+			</h1>
+		</section>
+	);
 }
 
 export default Genre;
