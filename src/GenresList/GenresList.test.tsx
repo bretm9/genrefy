11f113b8ed38bd4b439b0aa6dff3044d09d047d4
Bret Merritt <merrittbret9@gmail.com>
2020-11-01 13:24:31 -0700

    Add testing for GenresList

---
+++ src/GenresList/GenresList.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import GenresList from './GenresList';
+
+const mockSetAppGenre = jest.fn();
+
+describe('GenresList', () => {
+  const genres = [
+    'jam dubstep',
+    'demo indonesian ambient',
+    'trumpet orchestral'
+  ]
+	beforeEach(() =>
+		render(
+			<GenresList setAppGenre={mockSetAppGenre} genres={genres} />
+		)
+	);
+	test('should render the genresList container to the screen', () => {
+    expect(screen.getByText('jam dubstep')).toBeInTheDocument();
+    expect(screen.getByText('demo indonesian ambient')).toBeInTheDocument();
+    expect(screen.getByText('trumpet orchestral')).toBeInTheDocument();
+	});
+});
