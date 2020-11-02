b19aa2284c485264668f0f99b093762bc607bf0e
Nathan Darrington <npdarrington@icloud.com>
2020-11-02 14:23:42 -0700

    Added logo-container that will hold image and Genrefy title tag

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -3,10 +3,15 @@ import { Link } from 'react-router-dom'
 
 import './Header.scss'
 
+import logo from '../images/genrefy-logo.png';
+
 function Header() {
   return (
     <section className='header'>
-      <Link to='/'><article className="logo"></article></Link>
+      <article className='logo-container'>
+        <Link to='/'><img className='logo' src={logo} alt="Genrefy"/></Link>
+        <h1>Genrefy</h1>
+      </article>
       <Link to='/saved-playlists'><h1>Saved Playlist</h1></Link>
     </section>
   )
