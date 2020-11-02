878560dc29cccefc2beb466f5852d227d8297a8c
Nathan Darrington <npdarrington@icloud.com>
2020-11-02 09:03:53 -0700

    Added logo to screen and added initial styling for it

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
+import './Header.scss'
+
 function Header() {
   return (
     <section className='header'>
+      {/* <img src="../images/genrefy-logo.png" alt="Genrefy"/> */}
+      <article className="logo"></article>
       <h1>Header</h1>
     </section>
   )
