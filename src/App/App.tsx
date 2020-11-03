f6fa2e3013c939f2809e49299830fd7825cdc3ba
Nathan Darrington <npdarrington@icloud.com>
2020-11-03 09:34:24 -0700

    Formatted App code for better readability on longer components that have many props

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -5,40 +5,40 @@ import { Route } from 'react-router-dom';
 import Header from '../Header/Header';
 import GenresList from '../GenresList/GenresList';
 import PlaylistContainer from '../PlaylistContainer/PlaylistContainer';
-import PlaylistDetails from '../PlaylistDetails/PlaylistDetails'
+import PlaylistDetails from '../PlaylistDetails/PlaylistDetails';
 
 import { getPlaylist, getGenres } from '../apiCalls';
 
 import { Playlist } from '../utils';
 
-interface IProps { }
+interface IProps {}
 interface IState {
-	genres: [],
-	playlists: Playlist[] | [],
-	selectedGenre: string
+	genres: [];
+	playlists: Playlist[] | [];
+	selectedGenre: string;
 }
 
 class App extends Component<IProps, IState> {
 	constructor(props: any) {
-		super(props)
+		super(props);
 		this.state = {
 			genres: [],
 			playlists: [],
-			selectedGenre: ""
-		}
+			selectedGenre: '',
+		};
 	}
 
 	componentDidMount = () => {
-		getGenres()
-			.then(data => this.setState({ genres: data }))
-	}
+		getGenres().then(data => this.setState({ genres: data }));
+	};
 
 	setAppGenre = (genre: string) => {
 		this.setState({ selectedGenre: genre });
 		const splitGenreArray = this.parseGenreForFetch(genre);
-		getPlaylist(splitGenreArray)
-			.then(data => this.setState({ playlists: [...this.state.playlists, data] }));
-	}
+		getPlaylist(splitGenreArray).then(data =>
+			this.setState({ playlists: [...this.state.playlists, data] })
+		);
+	};
 
 	parseGenreForFetch = (genre: string) => {
 		const fillerWordsToRemove = /(the |and |of |\/| |ism)+/;
@@ -53,49 +53,60 @@ class App extends Component<IProps, IState> {
 			}
 		});
 		return parseGenreArray;
-	}
+	};
 
 	render() {
 		return (
 			<div className='App'>
 				<Header />
-				<Route exact path='/' render={() => {
-					return (
-						<section className="app-main">
-							<GenresList setAppGenre={this.setAppGenre} genres={this.state.genres} />
-							<PlaylistContainer playlistType={'generated-playlist'} selectedGenre={this.state.selectedGenre} playlists={this.state.playlists} />
-						</section>
-					)
-				}} />
-				<Route 
+				<Route
+					exact
+					path='/'
+					render={() => {
+						return (
+							<section className='app-main'>
+								<GenresList
+									setAppGenre={this.setAppGenre}
+									genres={this.state.genres}
+								/>
+								<PlaylistContainer
+									playlistType={'generated-playlist'}
+									selectedGenre={this.state.selectedGenre}
+									playlists={this.state.playlists}
+								/>
+							</section>
+						);
+					}}
+				/>
+				<Route
 					path='/saved'
 					render={() => {
-						const savedPlaylists: Playlist[] | [] = this.state.playlists.filter(playlist => playlist.isSaved)
+						const savedPlaylists: Playlist[] | [] = this.state.playlists.filter(
+							playlist => playlist.isSaved
+						);
 						return (
 							<PlaylistContainer
 								playlistType={'generated-playlist'}
 								selectedGenre={this.state.selectedGenre}
-								playlists={savedPlaylists} 
-							/>
-						)
-					}}
-				/>
-				<Route 
-					path='/playlist/:id' 
-					render={({ match }) => {
-						const playlistId: number = +match.params.id;
-						const foundPlaylist: Playlist | undefined = this.state.playlists.find(playlist => {
-							return playlist.id === playlistId
-						});
-						return (
-							<PlaylistDetails
-								playlist={foundPlaylist} 
+								playlists={savedPlaylists}
 							/>
 						);
 					}}
 				/>
+				<Route
+					path='/playlist/:id'
+					render={({ match }) => {
+						const playlistId: number = +match.params.id;
+						const foundPlaylist:
+							| Playlist
+							| undefined = this.state.playlists.find(playlist => {
+							return playlist.id === playlistId;
+						});
+						return <PlaylistDetails playlist={foundPlaylist} />;
+					}}
+				/>
 			</div>
-		)
+		);
 	}
 }
 
