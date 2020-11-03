7de7c37bf37cb268a4745acfc438fb99f3875108
Nathan Darrington <npdarrington@icloud.com>
2020-11-03 10:17:50 -0700

    Changed to new images instead of components and conditional render them based on isSaved. Formatted file

--- src/GeneratedPlaylists/GeneratedPlaylist.tsx
+++ src/GeneratedPlaylists/GeneratedPlaylist.tsx
@@ -2,37 +2,50 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Playlist } from '../utils';
 
-import { ReactComponent as SavedPlaylist } from '../images/star-active.svg';
-import { ReactComponent as UnsavedPlaylist } from '../images/star.svg';
+import savedPlaylist from '../images/saved-playlist.png';
+import unSavedPlaylist from '../images/unsaved-playlist.png';
 
 import './GeneratedPlaylist.scss';
 
-interface IProps { selectedGenre: string, playlist: Playlist, toggleSaved: (playlist: Playlist) => void }
+const savedPlaylistAlt = 'Save playlist';
+const unSavedplaylistAlt = 'Unsave playlist';
+
+interface IProps {
+	selectedGenre: string;
+	playlist: Playlist;
+	toggleSaved: (playlist: Playlist) => void;
+}
 
 const displaySongs = (playlist: Playlist) => {
-  return playlist.tracks.reduce((finalSongs: JSX.Element[], currentSong, index: number): JSX.Element[] => {
-      const songToDisplay = 
-        <section key={index}>
-          <h3>{currentSong.songName}</h3>
-        </section>;
-      finalSongs.push(songToDisplay)
-    return finalSongs
-  }, [])
-}
+	return playlist.tracks.reduce(
+		(finalSongs: JSX.Element[], currentSong, index: number): JSX.Element[] => {
+			const songToDisplay = (
+				<section key={index}>
+					<h3>{currentSong.songName}</h3>
+				</section>
+			);
+			finalSongs.push(songToDisplay);
+			return finalSongs;
+		},
+		[]
+	);
+};
 
 const GeneratedPlaylist = (props: IProps) => {
-  return (
-    <section className="generated-playlist">
-      <article className="generated-playlist-head">
-        <Link to={`/playlist/${props.playlist.id}`}>{props.playlist.name}</Link>
-        { props.playlist.isSaved ? 
-            <SavedPlaylist className='save saved-playlist' onClick={() => props.toggleSaved(props.playlist)} /> : 
-            <UnsavedPlaylist className='save unsaved-playlist' onClick={() => props.toggleSaved(props.playlist)} /> 
-        }
-      </article>
-      {displaySongs(props.playlist)}
-    </section>
-  )
-}
+	return (
+		<section className='generated-playlist'>
+			<article className='generated-playlist-head'>
+				<Link to={`/playlist/${props.playlist.id}`}>{props.playlist.name}</Link>
+				<img
+					src={props.playlist.isSaved ? savedPlaylist : unSavedPlaylist}
+					className='save'
+					alt={props.playlist.isSaved ? unSavedplaylistAlt : savedPlaylistAlt}
+					onClick={() => props.toggleSaved(props.playlist)}
+				/>
+			</article>
+			{displaySongs(props.playlist)}
+		</section>
+	);
+};
 
 export default GeneratedPlaylist;
